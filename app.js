8a6e602e8b9b762895a16c82feebddbc50e26c48
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-08-14 22:05:42 -0400

    cria as variáveis de acesso ao DOM

---
+++ app.js
@@ -0,0 +1,6 @@
+const formulario = document.getElementById("form");
+const cidadeBuscada = document.getElementById("cidade-busca");
+const cidade = document.querySelector(".cidade");
+const grau = document.querySelector(".grau");
+const imagemTempo = document.getElementById("img-tempo");
+const situacaoTempo = document.querySelector(".p-situacao");
