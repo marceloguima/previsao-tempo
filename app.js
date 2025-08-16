15428f9ef375af6414cbaa0ee6fce102139622d5
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-08-16 19:34:40 -0400

    troca imagens de fundo conforme o tempo e cria uma card para uma msg no topo junto com  o form

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const temp = document.querySelector(".temp");
 const tempMax = document.querySelector(".t-max");
 const tempMim = document.querySelector(".t-mim");
 const velVento = document.querySelector(".v-vento");
+const titulo = document.querySelector(".titulo")
 
 let situacaoTempo = document.querySelector(".p-situacao");
 
 const kei = "42e7199bda86e079fb5097d63be93d2a";
 
-const meuBody = document.getElementById("body")
-
+const meuBody = document.getElementById("body");
 
 async function buscaCidade(cidadeBuscada) {
     const dados = await fetch(
@@ -22,25 +22,37 @@ async function buscaCidade(cidadeBuscada) {
     console.log(dados);
 }
 
+const containerDados = document.querySelector(".container-info");
 const renderizaDados = (dados) => {
     cidade.textContent = `Tempo em ${dados.name}`;
     temp.textContent = dados.main.temp.toFixed(0) + "°C";
     situacaoTempo.textContent = dados.weather[0].description;
-    tempMax.textContent = `Temp. Max ${dados.main.temp_max.toFixed(0)}`;
-    tempMim.textContent = `Temp. Mim ${dados.main.temp_min.toFixed(0)}`;
+    tempMax.textContent = `Temp. Max. ${dados.main.temp_max.toFixed(0)}`;
+    tempMim.textContent = `Temp. Min. ${dados.main.temp_min.toFixed(0)}`;
 
-    const ventoEmKmHora = dados.wind.speed * 3.6
-    velVento.textContent = `Vento ${ventoEmKmHora.toFixed(2)}km/h`
+  
+    const ventoEmKmHora = dados.wind.speed * 3.6;
+    velVento.textContent = `Vento ${ventoEmKmHora.toFixed(2)}km/h`;
 
     umidadeAr.textContent = `Umidade ${dados.main.humidity}%`;
 
-    if(dados.weather[0].description === "nublado"){
-meuBody.style.backgroundImage = "url(./image/nublado.jpg)"
-    }else if(dados.weather[0].description === "nuvens dispersas"){
-        meuBody.style.backgroundImage = "url(./image/nuvens-dispersas.jpg)"
-    }else{
-         meuBody.style.backgroundImage = "url(./image/chuva.jpg)"
+    if (dados.weather[0].description === "nublado") {
+        meuBody.style.backgroundImage = "url(./image/nublado.jpg)";
+        // quando nublado muda o título pra branco por motivos de contraste
+        titulo.style.color = "#fff"
+
+    } else if (dados.weather[0].description === "nuvens dispersas") {
+        meuBody.style.backgroundImage = "url(./image/nuvens-dispersas.jpg)";
+
+    } else if (dados.weather[0].description === "névoa" || "neblina") {
+        meuBody.style.backgroundImage = "url(./image/nevoa.jpg)";
+
+    } else if (dados.weather[0].description === "chuva") {
+        meuBody.style.backgroundImage = "url(./image/chuva.jpg)";
     }
+
+    // mostra o container com dados estilizados
+    containerDados.style.display = "block";
 };
 
 formulario.addEventListener("submit", (evento) => {
@@ -48,5 +60,3 @@ formulario.addEventListener("submit", (evento) => {
     const cidadeBuscada = document.getElementById("cidade-busca").value;
     buscaCidade(cidadeBuscada);
 });
-
-
